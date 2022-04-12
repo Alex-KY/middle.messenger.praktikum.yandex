4cbeee93a346e6818351b221018d9e77f05f70bf
Alexey Kravchenko <a.kravchenko@rt-solar.ru>
2022-04-12 22:16:35 +0300

    Этап 5. Docker

--- server.ts
+++ server.ts
@@ -3,9 +3,9 @@ const PORT = 3000;
 
 const app = express();
 
-app.use(express.static('dist'));
+app.use(express.static('./dist'));
 
-app.use('/*', express.static('dist'));
+app.use('/*', express.static('./dist'));
 
 app.listen(process.env.PORT || PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
